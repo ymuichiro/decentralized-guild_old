1a43445f26bfbfb51081c79be84c1c0f035cb51c
karriz-dev <karriz@xymfor.us>
2022-10-31 10:36:27 +0900

    [fix] downgrade `symbol-sdk` 2.0.3->2.0.2

--- frontend/src/pages/app/Dashboard.tsx
+++ frontend/src/pages/app/Dashboard.tsx
@@ -1,5 +1,12 @@
+import Container from '@components/atom/Container';
+import Typography from '@components/atom/Typography';
+
 const Dashboard = (): JSX.Element => {
-  return <div></div>;
+  return (
+    <Container maxWidth='md'>
+      <Typography>Dashboard</Typography>
+    </Container>
+  );
 };
 
 export default Dashboard;
