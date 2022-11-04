2e32c2955fd0e3b86a88f39dde63f9fb5e0fd04e
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2022-11-04 17:46:15 +0900

    [add] ユーザーの登録状況を検証するAPIを追加

--- backend/src/services/System.ts
+++ backend/src/services/System.ts
@@ -4,10 +4,10 @@ import {
   SignedTransaction,
   AggregateTransaction,
   TransferTransaction,
-  Account, 
+  Account,
   NetworkType,
   PublicAccount,
-  EncryptedMessage
+  EncryptedMessage,
 } from 'symbol-sdk';
 
 export type VerifiedSss = {
@@ -21,7 +21,7 @@ export class System {
   static cosignateBySystem(signedTransaction: SignedTransaction): CosignatureSignedTransaction {
     try {
       const verify = this.verifyTransaction(signedTransaction);
-      if(!verify) throw new Error('不正なトランザクションの可能性があります')
+      if (!verify) throw new Error('不正なトランザクションの可能性があります');
       const systemAccount = Account.createFromPrivateKey(process.env.SYSTEM_PRIVATEKEY!, signedTransaction.networkType);
       const cosignedSignedTransaction = CosignatureTransaction.signTransactionPayload(
         systemAccount,
@@ -34,45 +34,33 @@ export class System {
     }
   }
   // 正しいトランザクションか検証する。もっとちゃんとしたほうがいいけどとりあえず。
-  protected static verifyTransaction(signedTransaction: SignedTransaction){
+  protected static verifyTransaction(signedTransaction: SignedTransaction) {
     const aggregateTransaction = AggregateTransaction.createFromPayload(signedTransaction.payload);
-    const transferTransactions = aggregateTransaction.innerTransactions.filter((tx)=>{
-      return tx.type == 16724 && tx.signer?.publicKey == process.env.SYSTEM_PUBLICKEY
-    })
-    const mosaicAmounts = transferTransactions.filter((tx)=>{
-      (tx as TransferTransaction).mosaics.filter((m)=>{
-        return m.amount.compact() > 1
-      })
-    })
-    if(mosaicAmounts.length > 1) return false;
+    const transferTransactions = aggregateTransaction.innerTransactions.filter((tx) => {
+      return tx.type == 16724 && tx.signer?.publicKey == process.env.SYSTEM_PUBLICKEY;
+    });
+    const mosaicAmounts = transferTransactions.filter((tx) => {
+      (tx as TransferTransaction).mosaics.filter((m) => {
+        return m.amount.compact() > 1;
+      });
+    });
+    if (mosaicAmounts.length > 1) return false;
     return true;
   }
 
   /**
    * 渡されたTokenをSystemアカウントにより復号し、署名が行えるか確認する
    */
-  static verifyToken(
-    userPublicKey: string,
-    token: string,
-    network: NetworkType,
-  ): VerifiedSss {
+  static verifyToken(userPublicKey: string, token: string, network: NetworkType): VerifiedSss {
     try {
-      const userPublic = PublicAccount.createFromPublicKey(
-        userPublicKey,
-        network,
-      );
+      const userPublic = PublicAccount.createFromPublicKey(userPublicKey, network);
       const msg = new EncryptedMessage(token, userPublic);
-      const verifier = Account.createFromPrivateKey(
-        process.env.SYSTEM_PRIVATEKEY!,
-        network,
-      );
+      const verifier = Account.createFromPrivateKey(process.env.SYSTEM_PRIVATEKEY!, network);
       return JSON.parse(
-        verifier.decryptMessage(
-          msg,
-          PublicAccount.createFromPublicKey(userPublicKey, network),
-        ).payload,
+        verifier.decryptMessage(msg, PublicAccount.createFromPublicKey(userPublicKey, network)).payload,
       ) as VerifiedSss;
-    } catch {
+    } catch (e) {
+      console.error(e);
       throw new Error('署名検証に失敗しました');
     }
   }
