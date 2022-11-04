d2dc2bfa576e81df73097210effc5fd630935f3d
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2022-11-04 17:46:36 +0900

    [add] ログインページにロジック追加

--- frontend/src/service/ApiService.ts
+++ frontend/src/service/ApiService.ts
@@ -1,9 +1,31 @@
-import axios from 'axios';
+import { operations } from '../@types/swagger';
+import axios, { AxiosInstance } from 'axios';
 
-export const apiClient = axios.create({
-    baseURL: 'http://localhost:3001/',
+export class ApiService {
+  private static apiClient: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001/api',
     responseType: 'json',
     headers: {
-        'Content-Type': 'application/json',
+      'Content-Type': 'application/json',
     },
-});
+  });
+
+  /** SSS を用いてユーザー情報が正しいか検証する */
+  public static verifyUser(public_key: string, token: string) {
+    return this.apiClient.post<
+      never,
+      operations['verifyUser']['responses']['200']['content']['application/json'],
+      operations['verifyUser']['requestBody']['content']['application/json']
+    >('/user/verify', { public_key, token });
+  }
+
+  public static getUser(public_key: string) {
+    const params: operations['getUser']['parameters']['query'] = {
+      public_key,
+    };
+    return this.apiClient.get<
+      never,
+      operations['getUser']['responses']['200']['content']['application/json']
+    >('/user', { params });
+  }
+}
