412154d1262d07e5dd993aad183b84c2b8e16828
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2022-11-06 19:15:55 +0900

    [add] Error を出力するように修正

--- frontend/src/service/AuthService.ts
+++ frontend/src/service/AuthService.ts
@@ -10,7 +10,11 @@ export class AuthService extends SystemService {
       const token = await this.getActiveAccountToken();
       const { publicKey: public_key } = this.getActivePublicAccount();
       return await ApiService.verifyUser({ public_key, token });
-    } catch {
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(err);
+        console.error(err.message);
+      }
       throw new Error(
         '署名に失敗しました。やり直すか SSS の設定が正しいか確認下さい',
       );
